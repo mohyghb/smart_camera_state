7580c33f8181ae125c0189ba2b60725f5bdfd5ed
Moh Yaghoub <moh10moh78@gmail.com>
2023-02-25 21:45:48 -0800

    add an example

---
+++ example/lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:stream_camera_state/stream_camera_state.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends StreamCameraState<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: buildCameraPreview(
+        previewBuilder: (context, controller) {
+          return CameraPreview(controller);
+        },
+      ),
+    );
+  }
+
+  @override
+  void onCameraControllerInitialized() {
+    print('camera controller initialized');
+  }
+
+  @override
+  void onCameraImageAvailable(CameraImage cameraImage) {
+    print('image available');
+  }
+}
